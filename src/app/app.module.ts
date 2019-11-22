876ba80cc5a2c332c938ffbadebeddd9974f7eac
Arturo <totobronco@hotmail.com>
2019-11-22 15:41:57 -0300

    tutorial video 46

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HeroesComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
